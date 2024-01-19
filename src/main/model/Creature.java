54c379bffef170d995c1815a1c6229999f068078
“ArturChegur” <a0030954@mail.ru>
2024-01-20 01:18:07 +0300

    added main fields

--- src/main/model/Creature.java
+++ src/main/model/Creature.java
@@ -1,5 +1,8 @@
 package main.model;
 
 public abstract class Creature extends Entity {
+    protected Integer speed;
+    protected Integer health;
+
     public abstract void makeMove();
 }
--- src/main/model/dynamic/Predator.java
+++ src/main/model/dynamic/Predator.java
@@ -3,6 +3,7 @@ package main.model.dynamic;
 import main.model.Creature;
 
 public class Predator extends Creature {
+    private Integer damage;
     @Override
     public void makeMove() {
 
