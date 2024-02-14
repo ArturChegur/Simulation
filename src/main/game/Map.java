af1e8949f81b0af3a09b52b732580f01f2c47655
“ArturChegur” <a0030954@mail.ru>
2024-02-14 13:32:15 +0300

    starting project by default

--- src/main/game/Map.java
+++
@@ -1,7 +0,0 @@
-package main.game;
-
-public class Map {
-    private static final int HEIGHT = 10;
-    private static final int WIDTH = 20;
-
-}
--- src/main/rendering/Renderer.java
+++
@@ -1,4 +0,0 @@
-package main.rendering;
-
-public class Renderer {
-}
